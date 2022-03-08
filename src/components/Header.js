3ecb0e371fcbc717e74b76d60c223fb48260dca4
Keith <keith.francis83@gmail.com>
2022-03-08 15:15:51 -0700

    Created header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../componentsStyles/headerStyles.css';
+
+export default function Header() {
+  return(
+    <div className='header-displayBox'>
+      <h1 className='header-displayName'>Dollars Bank</h1>
+    </div>
+  );
+}
