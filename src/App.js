7d9687b2811a997a7c88d0be4c8279e0830591bb
Keith <keith.francis83@gmail.com>
2022-03-08 16:08:39 -0700

    Created pages files and built Login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className='atm-screen'>
       <Header />
+      <Login />
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import '../componentsStyles/loginStyles.css';
+
+export default function Login() {
+  const [warning, setWarning] = useState('none');
+
+  const checkUserData = () => {
+    (warning == 'none') ? setWarning('block') : setWarning('none');
+  }
+
+  useEffect(() => {
+
+  }, [warning])
+
+  return(
+    <React.Fragment>
+      <div className='login-div'>
+        <h2>Welcome to Dollars Bank!</h2>
+        <h2>Please enter your information.</h2>
+      </div>
+      <div className='login-userDiv'>
+        <h4>UserID</h4>
+        <input className='login-userInput' type='input' id='userid' />
+        <h4>PIN</h4>
+        <input className='login-userInput' type='input' id='pin' />
+        <button className='login-enter-btn' onClick={() => checkUserData()}>ENTER</button>
+      </div>
+      <h3 style={{display: warning}} className='login-invalid'>Incorrect UserID and PIN combination! Please try again.</h3>
+    </React.Fragment>
+  );
+}
